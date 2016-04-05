7ef3273a3708b74529d8e2ba026191d0ecd67ced
rsv-1985 <sergey.rasputniy@gmail.com>
2016-04-05 19:27:16 +0300

    1

--- application/views/themes/default/catalog/index.php
+++ application/views/themes/default/catalog/index.php
@@ -5,16 +5,16 @@
  * Email: sergey.rasputniy@gmail.com
  */
 
-defined('BASEPATH') OR exit('No direct script access allowed');?>
-<div class="row">
-    <div class="container">
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="container">
+    <div class="row">
         <div class="col-md-12">
-            <h1><?php echo $h1;?></h1>
+            <h1><?php echo $h1; ?></h1>
             <div class="catalog">
-                <?php if($manufacturers){?>
-                    <?php foreach($manufacturers as $manufacturer){?>
-                        <a href="/catalog/<?php echo $manufacturer['slug'];?>">
-                            <img src="<?php echo $manufacturer['logo'];?>" alt="<?php echo $manufacturer['name'];?>">
+                <?php if ($manufacturers) { ?>
+                    <?php foreach ($manufacturers as $manufacturer) { ?>
+                        <a href="/catalog/<?php echo $manufacturer['slug']; ?>">
+                            <img src="<?php echo $manufacturer['logo']; ?>" alt="<?php echo $manufacturer['name']; ?>">
                         </a>
                     <?php } ?>
                 <?php } ?>
