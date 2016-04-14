e2717654f9bc1c5471f787e0b33a618575f5e60f
rsv-1985 <sergey.rasputniy@gmail.com>
2016-04-14 23:58:14 +0300

    1

---
+++ application/migrations/001_first.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_first extends CI_Migration {
+
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+}
