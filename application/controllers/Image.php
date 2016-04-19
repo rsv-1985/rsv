31ab51548abf305edfbe226cb3268a9b8cd42ae4
rsv-1985 <sergey.rasputniy@gmail.com>
2016-04-19 21:36:03 +0300

    1

--- application/controllers/Image.php
+++ application/controllers/Image.php
@@ -67,7 +67,7 @@ class Image extends CI_Controller
             $file = theme_url().'img/no_image.png';
         }
 
-        $config['image_library'] = 'gd2';
+        $config['image_library'] = 'ImageMagick';
         $config['source_image'] = '.'.$file;
         $config['maintain_ratio'] = true;
         $config['dynamic_output'] = true;
