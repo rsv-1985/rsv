4b1883bb852f93961e5c0ce49b2ecbf519b42870
rsv-1985 <sergey.rasputniy@gmail.com>
2016-04-21 20:55:05 +0300

    1

--- application/libraries/Tecdoc.php
+++ application/libraries/Tecdoc.php
@@ -214,7 +214,8 @@ class Tecdoc {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $jsonurl);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 3);
+        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
         $res = curl_exec($curl);
         curl_close($curl);
 
