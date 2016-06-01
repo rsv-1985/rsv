72afab45753710c6e95374714fd625badbff3abf
rsv-1985 <sergey.rasputniy@gmail.com>
2016-06-01 21:37:49 +0300

    1

--- application/views/themes/default/footer.php
+++ application/views/themes/default/footer.php
@@ -62,7 +62,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
         <div class="row">
             <div class="col-md-12">
                 <div class="pull-right">
-                    Powered by <a href="http://cms.autoxcatalog.com/">cms.autoxcatalog.com <img onerror="imgError(this);" src="/assets/themes/default/img/autox.png"></a>
+                    Powered by <a href="http://cms.autoxcatalog.com/">cms.autoxcatalog.com <img onerror="imgError(this);" src="/assets/themes/default/img/autox.png" style="width: 67px;"></a>
                 </div>
             </div>
         </div>
