3a16262f699ed1a517f6b7dd6e17f6e13436745c
rsv-1985 <sergey.rasputniy@gmail.com>
2016-06-18 11:05:59 +0300

    1

--- application/controllers/autoxadmin/Index.php
+++ application/controllers/autoxadmin/Index.php
@@ -27,8 +27,8 @@ class Index extends Admin_controller{
         $this->load->helper('file');
         delete_files('./application/cache/web/');
         //Database optimeze
-        $this->load->dbutil();
-        $this->dbutil->optimize_database();
+        //$this->load->dbutil();
+        //$this->dbutil->optimize_database();
         $this->session->set_flashdata('success', lang('text_success_cache'));
         redirect('autoxadmin');
     }
