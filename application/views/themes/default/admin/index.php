3bdfad705ebd949f9141ef63c60124c2f09dc5f6
rsv-1985 <sergey.rasputniy@gmail.com>
2016-10-19 23:06:30 +0300

    1

--- application/views/themes/default/admin/index.php
+++ application/views/themes/default/admin/index.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
         <!-- Main content -->
         <section class="content">
             <div class="pull-right">
-                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcmsautox%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcmsautox%2F&tabs=timeline&width=440&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="440" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
             </div>
         </section><!-- /.content -->
 
