08d8c51dce8008c0244d8a514ee1a1dcded804b2
rsv-1985 <sergey.rasputniy@gmail.com>
2016-10-29 18:13:15 +0300

    1

---
+++ application/language/russian/admin/message_template_lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['text_heading'] = 'Шаблоны сообщений';
+$lang['text_subject'] = 'Тема сообщения';
+$lang['text_text'] = 'Текст email сообщения';
+$lang['text_sms'] = 'Текст sms сообщения';
