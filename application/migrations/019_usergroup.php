34558c9ba980f98c5d733b182feb73b53b47de55
rsv-1985 <sergey.rasputniy@gmail.com>
2016-11-27 19:39:28 +0200

    1

---
+++ application/migrations/019_usergroup.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_usergroup extends CI_Migration {
+
+    public function up()
+    {
+        $this->db->query("CREATE TABLE `cmsautox`.`ax_user_group` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(32) NOT NULL , `access` LONGTEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;");
+    }
+    public function down()
+    {
+        return;
+    }
+}
