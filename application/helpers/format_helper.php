280356ef7e5e6c70abce5937e22456079d561416
rsv-1985 <sergey.rasputniy@gmail.com>
2016-12-28 17:00:18 +0200

    1

--- application/helpers/format_helper.php
+++ application/helpers/format_helper.php
@@ -58,3 +58,7 @@ function plural_form($number, $after) {
     $cases = array (2, 0, 1, 1, 1, 2);
     return $number.' '.$after[ ($number%100>4 && $number%100<20)? 2: $cases[min($number%10, 5)] ];
 }
+
+function format_data($data){
+    return substr($data, 0, 4)."-".substr($data, 4, 2);
+}
