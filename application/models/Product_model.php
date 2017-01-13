764ce29ffc84fe1d012218aa14c2b975ca4a09f3
rsv-1985 <sergey.rasputniy@gmail.com>
2017-01-13 18:43:41 +0200

    1

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -58,7 +58,6 @@ class Product_model extends Default_model{
 (SELECT sku FROM ax_product WHERE id = product_id) as sku,
 (SELECT brand FROM ax_product WHERE id = product_id) as brand', false);
         $this->db->from('product');
-        $this->db->join('product_price','product_price.product_id = product.id');
         if($this->input->get()){
             if($this->input->get('sku')){
                 $this->db->where('sku', $this->input->get('sku',true));
