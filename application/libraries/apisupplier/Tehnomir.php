ec196541755fd2858f9aa53f468b5f3d6ec97572
rsv-1985 <sergey.rasputniy@gmail.com>
2017-03-14 19:19:16 +0200

    1

--- application/libraries/apisupplier/Tehnomir.php
+++ application/libraries/apisupplier/Tehnomir.php
@@ -25,7 +25,6 @@ class Tehnomir{
         //Удаляем все ценовые предложения перед поиском
         $this->CI->product_model->product_delete(['supplier_id' => (int)$supplier_id]);
 
-        $soapUrl = 'http://tehnomir.com.ua/ws/soap.wsdl';
         $login = $this->CI->config->item('api_tehnomir_login');;
         $password = $this->CI->config->item('api_tehnomir_password');
         $currency = $this->CI->config->item('api_tehnomir_currency');
@@ -33,6 +32,81 @@ class Tehnomir{
 
         $url = 'https://www.tehnomir.com.ua/ws/xml.php?act=GetPriceWithCrosses&usr_login='.$login.'&currency='.$currency.'&usr_passwd='.$password.'&PartNumber='.$sku;
 
+        $results = $this->get_products($url);
+        if(isset($results['QueryStatus']) && $results['QueryStatus']['QueryStatusCode'] == 1){
+            if($brand){
+                foreach ($results['Producers']['Producer'] as $producer){
+                    if($producer['Brand'] == $brand){
+                        $url = 'https://www.tehnomir.com.ua/ws/xml.php?act=GetPriceWithCrosses&usr_login='.$login.'&currency='.$currency.'&usr_passwd='.$password.'&PartNumber='.$sku.'&BrandId='.$producer['BrandId'];
+                        $results = $this->get_products($url);
+                        break;
+                    }
+                }
+            }else{
+                $url = 'https://www.tehnomir.com.ua/ws/xml.php?act=GetPriceWithCrosses&usr_login=' . $login . '&currency=' . $currency . '&usr_passwd=' . $password . '&PartNumber=' . $sku . '&BrandId=' . $results['Producers']['Producer'][0]['BrandId'];
+                $results = $this->get_products($url);
+            }
+
+        }
+
+        if($this->CI->input->get('debug_show')){
+            echo '<pre>';
+            print_r($results);
+            echo '</pre>';
+        }
+        if(isset($results['QueryStatus']) && $results['QueryStatus']['QueryStatusCode'] == 0 && count($results['Prices']) > 0){
+
+            $system_currency_id = 0;
+            foreach ($this->CI->currency_model->currencies as $system_currency){
+                if($system_currency['code'] == $currency){
+                    $system_currency_id = $system_currency['id'];
+                }
+            }
+
+
+            foreach ($results['Prices']['Price'] as $result) {
+
+                    $product = [
+                        'name' => $result['PartDescriptionRus'],
+                        'sku' => $result['PartNumberShort'],
+                        'brand' => $result['Brand'],
+                        'delivery_price' => $result['Price'],
+                        'quantity' => $result['Quantity'] == 0 ? 1 : $result['Quantity'],
+                        'supplier_id' => (int)$supplier_id,
+                        'term' => $result['DeliveryDays'] * 24 + (int)$plus_day
+                    ];
+
+                    $product_data = [
+                        'sku' => $product['sku'],
+                        'brand' => $product['brand'],
+                        'name' => $product['name'],
+                        'slug' => $this->CI->product_model->getSlug($product),
+                    ];
+
+                    $product_id = $this->CI->product_model->product_insert($product_data, false, false);
+
+                    $price_data[] = [
+                        'product_id' => $this->CI->db->escape($product_id),
+                        'excerpt' => $this->CI->db->escape(''),
+                        'currency_id' => $this->CI->db->escape($system_currency_id),
+                        'delivery_price' => $this->CI->db->escape($product['delivery_price']),
+                        'saleprice' => $this->CI->db->escape(0),
+                        'quantity' => $this->CI->db->escape($product['quantity']),
+                        'supplier_id' => $this->CI->db->escape($supplier_id),
+                        'term' => $this->CI->db->escape($product['term']),
+                        'created_at' => $this->CI->db->escape(date("Y-m-d H:i:s")),
+                        'updated_at' => $this->CI->db->escape(date("Y-m-d H:i:s")),
+                        'status' => true,
+                    ];
+            }
+
+            if(@$price_data){
+                $this->CI->product_model->price_insert($price_data);
+            }
+        }
+    }
+
+    public function get_products($url){
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -42,61 +116,7 @@ class Tehnomir{
         if($res != ''){
             $xml = simplexml_load_string($res);
             $json = json_encode($xml);
-            $results = json_decode($json,TRUE);
-            if($this->CI->input->get('debug_show')){
-                print_r($results);
-            }
-            if(isset($results['QueryStatus']) && $results['QueryStatus']['QueryStatusCode'] == 0 && count($results['Prices']) > 0){
-
-                $system_currency_id = 0;
-                foreach ($this->CI->currency_model->currencies as $system_currency){
-                    if($system_currency['code'] == $currency){
-                        $system_currency_id = $system_currency['id'];
-                    }
-                }
-
-
-                foreach ($results['Prices']['Price'] as $result) {
-                    if ($result['Quantity'] > 0) {
-                        $product = [
-                            'name' => $result['PartDescriptionRus'],
-                            'sku' => $result['PartNumberShort'],
-                            'brand' => $result['Brand'],
-                            'delivery_price' => $result['Price'],
-                            'quantity' => $result['Quantity'],
-                            'supplier_id' => (int)$supplier_id,
-                            'term' => $result['DeliveryDays'] * 24 + (int)$plus_day
-                        ];
-
-                        $product_data = [
-                            'sku' => $product['sku'],
-                            'brand' => $product['brand'],
-                            'name' => $product['name'],
-                            'slug' => $this->CI->product_model->getSlug($product),
-                        ];
-
-                        $product_id = $this->CI->product_model->product_insert($product_data, false, false);
-
-                        $price_data[] = [
-                            'product_id' => $this->CI->db->escape($product_id),
-                            'excerpt' => $this->CI->db->escape(''),
-                            'currency_id' => $this->CI->db->escape($system_currency_id),
-                            'delivery_price' => $this->CI->db->escape($product['delivery_price']),
-                            'saleprice' => $this->CI->db->escape(0),
-                            'quantity' => $this->CI->db->escape($product['quantity']),
-                            'supplier_id' => $this->CI->db->escape($supplier_id),
-                            'term' => $this->CI->db->escape($product['term']),
-                            'created_at' => $this->CI->db->escape(date("Y-m-d H:i:s")),
-                            'updated_at' => $this->CI->db->escape(date("Y-m-d H:i:s")),
-                            'status' => true,
-                        ];
-                    }
-                }
-
-                if(@$price_data){
-                    $this->CI->product_model->price_insert($price_data);
-                }
-            }
+            return json_decode($json,TRUE);
         }
     }
 }
