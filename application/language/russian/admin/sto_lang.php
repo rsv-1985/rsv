1bcc9b39e5645f98a501047e4a727babcfb90397
rsv-1985 <sergey.rasputniy@gmail.com>
2017-03-19 22:25:56 +0200

    1

---
+++ application/language/russian/admin/sto_lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['text_heading'] = 'СТО';
+$lang['text_settings_heading'] = 'настройки';
