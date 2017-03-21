cd27e682b0985d64e925b8fac3bc2801ccda8be2
rsv-1985 <sergey.rasputniy@gmail.com>
2017-03-21 21:26:52 +0200

    1

--- updatesystem.php
+++ updatesystem.php
@@ -7,6 +7,15 @@
 exec('git pull', $output);
 
 if($output){
-    exit('Success!');
+    exit('Success!<br><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Адаптивный1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9020295635568297"
+     data-ad-slot="6031374792"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>');
 }
 exit('Error');
