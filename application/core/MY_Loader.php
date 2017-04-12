8b2f65f82ec548201b7c347847501e3ee5d958cb
rsv-1985 <sergey.rasputniy@gmail.com>
2017-04-12 11:41:37 +0300

    1

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -11,6 +11,6 @@ class MY_loader extends CI_Loader{
     public function view($view, $vars = array(), $return = FALSE)
     {
         $CI = &get_instance();
-        return $this->_ci_load(array('_ci_view' => 'themes/'.$CI->config->item('theme').'/'.$view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
+        return $this->_ci_load(array('_ci_view' => 'themes/'.$CI->config->item('theme').'/'.$view, '_ci_vars' => $this->_ci_prepare_view_vars($vars), '_ci_return' => $return));
     }
 }
