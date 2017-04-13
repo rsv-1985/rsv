803b626fed9aec13d9b027197061dd54fc078abb
rsv-1985 <sergey.rasputniy@gmail.com>
2017-04-13 22:40:00 +0300

    1

--- application/views/themes/default/email/order.php
+++ application/views/themes/default/email/order.php
@@ -7,27 +7,6 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');?>
 
-<table cellspacing="0" class="shop_table cart">
-    <thead>
-    <tr>
-        <th class="product-name"><?php echo lang('text_product');?></th>
-        <th class="product-price"><?php echo lang('text_price');?></th>
-        <th class="product-quantity"><?php echo lang('text_quantity');?></th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach($products as $key => $item){?>
-        <tr class="cart_item" id="<?php echo $key;?>">
-            <td>
-                <a href="<?php echo base_url();?>product/<?php echo $item['slug'];?>"><?php echo $item['sku'].' '.$item['brand'].' '.$item['name'];?></a>
-            </td>
-            <td>
-                <span class="amount"><?php echo format_currency($item['price']);?></span>
-            </td>
-            <td>
-              <?php echo $item['quantity'];?>
-            </td>
-        </tr>
-    <?php } ?>
-    </tbody>
-</table>
+    <?php $q = 1; foreach($products as $key => $item){?>
+       <?php echo lang('text_product').':'.$q;?>:<?php echo $item['sku'];?>===<?php echo $item['brand'];?>===<?php echo $item['quantity'].lang('text_st');?>===<?php echo format_currency($item['price']);?>===<?php echo format_currency($item['price']*$item['quantity']);?>
+    <?php $q++;} ?>
