8d0b239b60cd1d1eed9c9e3487dacbcfa4f7e0f2
rsv-1985 <sergey.rasputniy@gmail.com>
2017-06-01 11:49:35 +0300

    1

--- application/views/themes/default/email/order.php
+++ application/views/themes/default/email/order.php
@@ -8,5 +8,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');?>
 
     <?php $q = 1; foreach($products as $key => $item){?>
-       <?php echo lang('text_product').$q;?>:<?php echo $item['sku'];?>===<?php echo $item['brand'];?>===<?php echo $item['quantity'].lang('text_st');?>===<?php echo format_currency($item['price']);?>===<?php echo format_currency($item['price']*$item['quantity']);?><br/>
+       <?php echo lang('text_product').$q;?>:<?php echo $item['sku'];?>===<?php echo $item['brand'];?>===<?php echo $item['quantity'].lang('text_st');?>===<?php echo format_currency($item['price']);?>===<?php echo format_currency($item['price']*$item['quantity']);?><?php if(isset($suppliers)){?>===<?php echo @$suppliers[$item['supplier_id']]['name'];?><?php } ?><br/>
     <?php $q++;} ?>
