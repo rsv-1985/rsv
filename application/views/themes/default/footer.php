c7945162558d85eadede6301980c6fcf13cdef5d
rsv-1985 <sergey.rasputniy@gmail.com>
2017-06-02 23:44:44 +0300

    1

--- application/views/themes/default/footer.php
+++ application/views/themes/default/footer.php
@@ -191,7 +191,6 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
     </div>
 </div>
     <link href='https://fonts.googleapis.com/css?family=Play:400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/ico">
     <link rel="stylesheet" href="<?php echo theme_url();?>css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo theme_url();?>css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo theme_url();?>css/owl.carousel.css">
