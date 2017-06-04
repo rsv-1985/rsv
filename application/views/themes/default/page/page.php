1715588b1eaddc5a85f471d894bcf21ce2003f06
rsv-1985 <sergey.rasputniy@gmail.com>
2017-06-04 23:14:06 +0300

    1

--- application/views/themes/default/page/page.php
+++ application/views/themes/default/page/page.php
@@ -22,30 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
     <div class="zigzag-bottom"></div>
     <div class="container">
         <div class="row-fluid">
-            <?php if ($main || $parent){?>
-                <div class="col-md-4">
-                    <div class="single-sidebar">
-                        <ul>
-                            <?php if($main){?>
-                                <li><a href="/page/<?php echo $main['slug'];?>"><i class="fa fa-mail-reply"></i> <?php echo !empty($main['menu_title']) ? $main['menu_title'] : $main['name'];?></a></li>
-                            <?php } ?>
-                            <?php if($parent){?>
-                                <?php foreach($parent as $parent){?>
-                                    <?php if(!$this->is_login && $parent['show_for_user']){continue;}?>
-                                    <li><a href="/page/<?php echo $parent['slug'];?>"><?php echo !empty($parent['menu_title']) ? $parent['menu_title'] : $parent['name'];?></a></li>
-                                <?php } ?>
-                            <?php } ?>
-                        </ul>
-                    </div>
-                    <?php echo $this->load->view('form/category', null, true);?>
-
-                </div>
-            <?php } ?>
-            <?php if ($main || $parent){?>
-                <div class="col-md-8">
-                <?php }else{?>
-                <div class="col-md-12">
-                <?php } ?>
+            <div class="col-md-12">
                 <?php echo $description;?>
             </div>
         </div>
