41c802034ec13b13a0d15c08e3680b9a83648dde
rsv-1985 <sergey.rasputniy@gmail.com>
2017-09-02 20:30:12 +0300

    1

--- application/helpers/format_helper.php
+++ application/helpers/format_helper.php
@@ -7,7 +7,7 @@
 
 function format_currency($value){
     $CI = &get_instance();
-    $formated = number_format(abs($value), $CI->currency_model->default_currency['decimal_place'], '.','');
+    $formated = number_format($value, $CI->currency_model->default_currency['decimal_place'], '.','');
     return
         $CI->currency_model->default_currency['symbol_left']
         .$formated
