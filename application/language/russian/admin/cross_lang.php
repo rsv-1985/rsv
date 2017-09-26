d7a50bb9ad4929f86eac5c2a85ca71a7e3b80075
rsv-1985 <sergey.rasputniy@gmail.com>
2017-09-26 19:58:53 +0300

    1

--- application/language/russian/admin/cross_lang.php
+++ application/language/russian/admin/cross_lang.php
@@ -10,5 +10,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['text_heading'] = 'Свои кросс номера';
 $lang['text_success'] = 'Все прошло успешно';
 $lang['button_delete_all'] = 'Удали все';
-$lang['text_import'] = 'Импорт с файла xls';
+$lang['text_import'] = 'Импорт с файла xls,csv';
 $lang['text_template'] = 'Скачать пример';
