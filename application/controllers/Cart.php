6aa3a259ab7b20fdb164255b1cf5d2f11a21ea14
Sergey.Rasputniy@gmail.com <0618dfDF>
2017-11-01 12:38:29 +0200

    1

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -37,7 +37,7 @@ class Cart extends Front_controller
         if($this->is_login){
             $data['customer'] = $this->customer_model->get($this->is_login);
         }
-        if($this->input->post()){
+        if($this->input->post() && $this->cart->total_items() > 0){
             $this->form_validation->set_rules('delivery_method', lang('text_delivery_method'), 'required|integer');
             $this->form_validation->set_rules('payment_method', lang('text_payment_method'), 'required|integer');
             $this->form_validation->set_rules('first_name', lang('text_first_name'), 'required|max_length[250]');
