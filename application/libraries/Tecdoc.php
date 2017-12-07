027b89c31015656f252c2528bcd8e927957f5805
Sergey.Rasputniy@gmail.com <0618dfDF>
2017-12-07 13:30:09 +0200

    1

--- application/libraries/Tecdoc.php
+++ application/libraries/Tecdoc.php
@@ -193,7 +193,15 @@ class Tecdoc
 
     public function getIDart($article, $brand)
     {
-        $brand = str_replace(['-'], [''], preg_replace("/[^a-zA-ZА-Яа-я0-9]/", '', mb_strtoupper(trim($brand), 'UTF-8')));
+        $search = [
+            'LEMFORDER',
+            '-',
+        ];
+        $replace = [
+            'LEMFÖRDER',
+            ''
+        ];
+        $brand = str_replace($search, $replace, preg_replace("/[^a-zA-ZА-Яа-я0-9]/", '', mb_strtoupper(trim($brand), 'UTF-8')));
         $query = [
             'apikey' => $this->key,
             'method' => 'getSearch',
