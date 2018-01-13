196d3ee9bda69d72a6280964999792ebffc7f36f
rsv-1985 <sergey.rasputniy@gmail.com>
2018-01-13 23:40:47 +0200

    1

--- application/views/themes/default/admin/waybill/print.php
+++ application/views/themes/default/admin/waybill/print.php
@@ -7,27 +7,45 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <html>
+<style>
+    table{
+        border: 2px solid black;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    thead{
+        font-weight: bold;
+    }
+
+    .customer{
+        background: #D0D0D0;
+    }
+
+    .customer td{
+        border-top: 2px solid black;
+    }
+
+</style>
 <body onload="print()">
 <div>
     <?php if ($parcels) { ?>
-        <table>
+        <table id="t01">
             <tbody>
-            <?php foreach ($parcels
-
-            as $parcel){
-            $products_total = 0; ?>
             <thead>
-            <tr>
-                <th>Посылка</th>
-                <th>ФИО</th>
-                <th>Телефон</th>
-                <th>Способ доставки</th>
-                <th>Способ оплаты</th>
-                <th>Адрес доставки</th>
-                <th>ТТН</th>
+            <tr style="text-align: center;">
+                <td>№</td>
+                <td>ФИО</td>
+                <td>Телефон</td>
+                <td>Доставка</td>
+                <td>Оплата</td>
+                <td>Адрес</td>
+                <td>ТТН</td>
             </tr>
             </thead>
-            <tr>
+            <?php foreach ($parcels as $parcel){
+            $products_total = 0; ?>
+            <tr class="customer" style="text-align: center">
                 <td><?php echo $parcel['id']; ?></td>
                 <td>
                     <?php echo $parcel['first_name'] . ' ' . $parcel['last_name'] . ' ' . $parcel['patronymic']; ?><br>
@@ -42,16 +60,14 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                 <td><?php echo $parcel['ttn']; ?></td>
             </tr>
             <?php if ($parcel['products']) { ?>
-
-                    <tr style="font-weight: bold;">
+                    <tr style="text-align: center">
                         <td>Артикул</td>
                         <td>Бренд</td>
                         <td>Название запчасти</td>
                         <td>Количество</td>
                         <td>Поставщик</td>
-                        <td>Итого</td>
+                        <td colspan="2">Итого</td>
                     </tr>
-
                 <?php foreach ($parcel['products'] as $product) { ?>
                     <tr>
                         <td><?php echo $product['sku']; ?></td>
@@ -59,18 +75,15 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                         <td><?php echo substr($product['name'], 0, 20); ?></td>
                         <td><?php echo $product['quantity']; ?>шт</td>
                         <td><?php echo $product['sname']; ?></td>
-                        <td><?php $products_total += $product['quantity'] * $product['price'];
+                        <td colspan="2"><?php $products_total += $product['quantity'] * $product['price'];
                             echo $product['quantity'] * $product['price']; ?></td>
+
                     </tr>
                 <?php } ?>
-
             <?php } ?>
             <tr>
-                <td colspan="6" style="border: 1px solid; text-align: right;">
+                <td colspan="7" style="border: 1px solid; text-align: right;">
                     <b>Итого: </b><?php echo $products_total; ?></td>
-            </tr>
-            <tr>
-                <td colspan="6"><hr style="border: 0px; width: 100%; margin: 20px;"></td>
             </tr>
             <?php } ?>
             </tbody>
