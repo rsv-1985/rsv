86eccb12ebb9e212ea82c62a6d95c07cd1e2270b
rsv-1985 <sergey.rasputniy@gmail.com>
2018-02-28 21:16:28 +0200

    1

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,6 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 $autoload['libraries'] = array(
-    'cart',
     'database',
     'email',
     'session',
