04fe2779fd13ceaa8b1f1460029d90408fd211f9
rsv-1985 <sergey.rasputniy@gmail.com>
2018-03-15 23:10:41 +0200

    1

--- application/views/themes/default/footer.php
+++ application/views/themes/default/footer.php
@@ -20,14 +20,14 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
             <div class="col-md-3 col-sm-6">
                 <?php echo form_open('/ajax/newsletter',['id' => 'newsletter']);?>
-                    <div class="form-group">
-                        <label><?php echo lang('text_heading_newsletter');?></label>
-                        <input type="email" name="email" class="form-control" placeholder="email" required>
-                        <div class="alert-danger"></div>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn-default" type="submit"><?php echo lang('button_newsletter');?></button>
-                    </div>
+                <div class="form-group">
+                    <label><?php echo lang('text_heading_newsletter');?></label>
+                    <input type="email" name="email" class="form-control" placeholder="email" required>
+                    <div class="alert-danger"></div>
+                </div>
+                <div class="form-group">
+                    <button class="btn-default" type="submit"><?php echo lang('button_newsletter');?></button>
+                </div>
                 <p><?php echo lang('text_newsletter');?></p>
                 </form>
             </div>
@@ -54,7 +54,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                             <li><i class="fa fa-envelope"></i> <a href="mailto:<?php echo $email;?>"> <?php echo $email;?></a></li>
                         <?php } ?>
                         <li><i class="fa fa-map-marker"></i> <?php echo $this->contacts['city'];?> <?php echo $this->contacts['address'];?></li>
-                    </ul>      
+                    </ul>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 g_map">
@@ -63,6 +63,15 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
         </div>
     </div>
 </div> <!-- End footer top area -->
+
+<!--PHONE BTN-->
+<div class="dws"  data-toggle="modal" data-target="#call-back-modal">
+    <div class="pulse">
+        <div class="bloc"></div>
+        <div class="phone"><i class="fa fa-phone" aria-hidden="true"></i></div>
+        <div class="text">Кнопка связи</div>
+    </div>
+</div>
 
 <div class="footer-bottom-area">
     <div class="container-fluid">
@@ -93,22 +102,22 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
                 <div class="pull-right" id="powered">
                     <?php if(@$this->contacts['copyright']){?>
-                        <?php echo $this->contacts['copyright'];?>
-                        <div style="display: none">
-                    <?php }else{ ?>
-                            <div>
-                    <?php }?>
-                    Powered by <a href="http://autox.pro" title="Создание интернет-магазина автозапчастей">Autox</a>
+                    <?php echo $this->contacts['copyright'];?>
+                    <div style="display: none">
+                        <?php }else{ ?>
+                        <div>
+                            <?php }?>
+                            Powered by <a href="http://autox.pro" title="Создание интернет-магазина автозапчастей">Autox</a>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div> <!-- End footer bottom area -->
-<!-- Modal login-->
-<div class="modal fade" id="login" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <?php echo form_open('/ajax/login',['id' => 'login_form']);?>
+    </div> <!-- End footer bottom area -->
+    <!-- Modal login-->
+    <div class="modal fade" id="login" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <?php echo form_open('/ajax/login',['id' => 'login_form']);?>
             <div class="modal-content">
                 <div class="product-big-title-area">
                     <div class="product-bit-title text-center">
@@ -135,44 +144,44 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                     <div class="clearfix"></div>
                 </div>
             </div>
-        </form>
-    </div>
-</div>
-<!-- Modal search-->
-<div class="modal fade bs-example-modal-lg" id="search_modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="product-big-title-area">
-                <div class="product-bit-title text-center">
-                    <strong id="search_query"></strong>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div id="popover"></div>
-                <div class="list-group" id="search_brand_list"></div>
-            </div>
-            <div class="col-md-9" style="overflow: auto; max-height: 600px">
-                <a href="#" class="pull-right" onclick="location.reload();return false;">Открыть в новом окне</a>
-                <div class="search_result">
-                    <div style="text-align: center;font-size: 24px;margin: 0 0 15px;"><?php echo lang('text_change_brand');?></div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
+            </form>
         </div>
     </div>
-</div>
-<!-- Modal call-back-->
-<div class="modal fade" id="call-back-modal" tabindex="-1" role="dialog" >
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="product-big-title-area">
-                <div class="product-bit-title text-center">
-                    <?php echo lang('text_call_back');?>
+    <!-- Modal search-->
+    <div class="modal fade bs-example-modal-lg" id="search_modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="product-big-title-area">
+                    <div class="product-bit-title text-center">
+                        <strong id="search_query"></strong>
+                    </div>
                 </div>
+                <div class="col-md-3">
+                    <div id="popover"></div>
+                    <div class="list-group" id="search_brand_list"></div>
+                </div>
+                <div class="col-md-9" style="overflow: auto; max-height: 600px">
+                    <a href="#" class="pull-right" onclick="location.reload();return false;">Открыть в новом окне</a>
+                    <div class="search_result">
+                        <div style="text-align: center;font-size: 24px;margin: 0 0 15px;"><?php echo lang('text_change_brand');?></div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
             </div>
-            <div class="modal-body">
-                <div class="alert alert-danger" role="alert" style="display: none"></div>
-                <?php echo form_open('/ajax/call_back',['id' => 'call_back_form']);?>
+        </div>
+    </div>
+    <!-- Modal call-back-->
+    <div class="modal fade" id="call-back-modal" tabindex="-1" role="dialog" >
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="product-big-title-area">
+                    <div class="product-bit-title text-center">
+                        <?php echo lang('text_call_back');?>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-danger" role="alert" style="display: none"></div>
+                    <?php echo form_open('/ajax/call_back',['id' => 'call_back_form']);?>
                     <div class="form-group">
                         <label><?php echo lang('text_call_back_name');?></label>
                         <input type="text" class="form-control" name="name" minlength="3" required>
@@ -184,12 +193,12 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                     <div class="form-group">
                         <button type="submit" class="pull-right"><?php echo lang('button_send');?></button>
                     </div>
-                <div class="clearfix"></div>
-                </form>
+                    <div class="clearfix"></div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
     <!-- Modal fast order-->
     <div class="modal fade" id="fast-order-modal" tabindex="-1" role="dialog" >
         <div class="modal-dialog" role="document">
@@ -231,27 +240,27 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
             <?php echo $this->options['style'];?>
         </style>
     <?php } ?>
-<script src="<?php echo theme_url();?>js/bootstrap.min.js"></script>
+    <script src="<?php echo theme_url();?>js/bootstrap.min.js"></script>
     <script src="<?php echo theme_url();?>js/jquery.maskedinput.min.js"></script>
-<script src="<?php echo theme_url();?>js/function.js?v7"></script>
-<script src="<?php echo theme_url();?>js/owl.carousel.min.js"></script>
-<script src="<?php echo theme_url();?>js/main.js?v6"></script>
-<script type="text/javascript" src="<?php echo theme_url();?>js/bxslider.min.js"></script>
-<script type="text/javascript" src="<?php echo theme_url();?>js/script.slider.js"></script>
-<script type="text/javascript">
-    $( document ).ready(function() {
-        $('a[href="/<?php echo $this->uri->uri_string(); ?>"]').parents('li').addClass('active');
-        $("[name='telephone']").mask("<?php echo @$this->options['phonemask'];?>");
-        $("[name='phone']").mask("<?php echo @$this->options['phonemask'];?>");
-    });
-</script>
-<?php if($this->config->item('my_script')){?>
-    <?php foreach ($this->config->item('my_script') as $script){?>
-        <?php echo @file_get_contents(base_url($script));?>
+    <script src="<?php echo theme_url();?>js/function.js?v7"></script>
+    <script src="<?php echo theme_url();?>js/owl.carousel.min.js"></script>
+    <script src="<?php echo theme_url();?>js/main.js?v6"></script>
+    <script type="text/javascript" src="<?php echo theme_url();?>js/bxslider.min.js"></script>
+    <script type="text/javascript" src="<?php echo theme_url();?>js/script.slider.js"></script>
+    <script type="text/javascript">
+        $( document ).ready(function() {
+            $('a[href="/<?php echo $this->uri->uri_string(); ?>"]').parents('li').addClass('active');
+            $("[name='telephone']").mask("<?php echo @$this->options['phonemask'];?>");
+            $("[name='phone']").mask("<?php echo @$this->options['phonemask'];?>");
+        });
+    </script>
+    <?php if($this->config->item('my_script')){?>
+        <?php foreach ($this->config->item('my_script') as $script){?>
+            <?php echo @file_get_contents(base_url($script));?>
+        <?php } ?>
     <?php } ?>
-<?php } ?>
     <?php if(ENVIRONMENT == 'development' || $this->input->get('debug_show')){
         $this->output->enable_profiler(TRUE);
     }?>
-</body>
-</html>
+    </body>
+    </html>
