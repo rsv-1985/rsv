be9f86f8975e37d35fb372c098fa9ea3fc29ea41
rsv-1985 <sergey.rasputniy@gmail.com>
2018-04-02 23:04:44 +0300

    1

--- application/models/Supplier_model.php
+++ application/models/Supplier_model.php
@@ -15,7 +15,7 @@ class Supplier_model extends Default_model{
     }
 
     public function supplier_get_all(){
-        $results = $this->db->get($this->table)->result_array();
+        $results = $this->db->order_by('name','ASC')->get($this->table)->result_array();
         $return = [];
         foreach($results as $result){
             $return[$result['id']] = $result;
