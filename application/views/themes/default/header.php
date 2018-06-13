ed95762cbf452c492608c791dc5fc8b57ab1bc37
rsv-1985 <sergey.rasputniy@gmail.com>
2018-06-13 18:22:03 +0300

    1

--- application/views/themes/default/header.php
+++ application/views/themes/default/header.php
@@ -17,7 +17,10 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
     <title><?php echo str_replace('"','',$this->title);?></title>
     <meta name="description" content="<?php echo str_replace('"','',$this->description);?>">
     <meta name="keywords" content="<?php echo str_replace('"','',$this->keywords);?>">
-    <script src="<?php echo theme_url();?>js/jquery-1.12.3.min.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-1.12.4.min.js"
+            integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+            crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="/favicon.ico" type="image/ico">
     <?php if($this->rel_prev){?>
         <link rel="prev" href="<?php echo $this->rel_prev;?>">
