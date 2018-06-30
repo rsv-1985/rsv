7a51e1b6a00a5aca41bf523bc29e7a5659843d70
rsv-1985 <sergey.rasputniy@gmail.com>
2018-06-30 18:46:03 +0300

    1

---
+++ application/views/themes/default/admin/delivery/np/view_form.php
@@ -0,0 +1,239 @@
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php echo form_open('/delivery/np/create_en',['method' => 'post', 'id' => 'np-form']);?>
+<input type="hidden" name="order_id" value="<?php echo $order_info['id'];?>">
+<input type="hidden" name="np_id" value="<?php echo $form_data['id'];?>">
+<input type="hidden" name="NewAddress" value="1">
+<input type="hidden" name="PaymentMethod" value="Cash">
+<input type="hidden" name="CargoType" value="Cargo">
+<input type="hidden" name="RecipientType" value="PrivatePerson">
+<input type="hidden" name="RecipientName" value="<?php echo $order_info['last_name'].' '.$order_info['first_name'].' '.$order_info['patronymic'];?>">
+<input type="hidden" name="RecipientsPhone" value="<?php echo $order_info['telephone'];?>">
+<input type="hidden" name="VolumeGeneral" value="">
+<input type="hidden" name="AdditionalInformation" value="<?php echo $order_info['id'];?>">
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Данные для отправки</div>
+            <div class="panel-body">
+                <div class="">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Поиск населенного пункта</label>
+                            <input type="text" onkeyup="getCity($(this).val())" class="form-control">
+                            <div class="city-res"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Город</label>
+                            <input type="text" name="RecipientCityName"
+                                   value="<?php echo @$form_data['RecipientCityName']; ?>" readonly class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Область</label>
+                            <input type="text" name="RecipientArea" value="<?php echo @$form_data['RecipientArea']; ?>"
+                                   readonly class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Регион</label>
+                            <input type="text" name="RecipientAreaRegions"
+                                   value="<?php echo @$form_data['RecipientAreaRegions']; ?>" readonly class="form-control">
+                        </div>
+                        <div class="form-group" id="RecipientAddressName"
+                             <?php if (@$form_data['RecipientAddressName2']){ ?>style="display: none"<?php } ?>>
+                            <label>Склад</label>
+                            <select readonly name="RecipientAddressName" class="form-control">
+                                <?php if (isset($form_data['RecipientAddressName'])) { ?>
+                                    <option value="<?php echo $form_data['RecipientAddressName']; ?>"
+                                            selected><?php echo $form_data['RecipientAddressName']; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div id="WarehouseDoors"
+                             <?php if (!$form_data['RecipientAddressName2']){ ?>style="display: none"<?php } ?>>
+
+                            <div class="form-group">
+                                <label>Адрес</label>
+                                <input value="<?php echo $form_data['RecipientAddressName2']; ?>" class="form-control"
+                                       type="text" name="RecipientAddressName2">
+                            </div>
+                            <div class="form-group">
+                                <label>Номер дома</label>
+                                <input value="<?php echo $form_data['RecipientHouse']; ?>" class="form-control" type="text"
+                                       name="RecipientHouse">
+                            </div>
+                            <div class="form-group">
+                                <label>Этаж</label>
+                                <input value="<?php echo $form_data['RecipientFlat']; ?>" class="form-control" type="text"
+                                       name="RecipientFlat">
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Тип доставки</label>
+                            <select name="ServiceType" class="form-control">
+                                <option value="WarehouseWarehouse"
+                                        <?php if (!$form_data['RecipientAddressName2']){ ?>selected<?php } ?>>Склад
+                                </option>
+                                <option value="WarehouseDoors"
+                                        <?php if ($form_data['RecipientAddressName2']){ ?>selected<?php } ?>>Адресная
+                                </option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Плательщик за доставку</label>
+                            <select name="PayerType" class="form-control">
+                                <option value="Recipient" selected>Получатель</option>
+                                <option value="Sender">Отправитель</option>
+                            </select>
+                        </div>
+
+
+
+
+                        <div class="form-group">
+                            <label>Вес</label>
+                            <input required type="number" name="Weight" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Количество мест</label>
+                            <input type="number" name="SeatsAmount" value="1" class="form-control">
+                        </div>
+
+
+
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Форма оплаты</label>
+                            <select name="nalojka" class="form-control">
+                                <option value="1">Наложенный платежь</option>
+                                <option value="0">Предоплата</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Сумма наложки</label>
+                            <input type="number" name="RedeliveryString" class="form-control" value="<?php echo $order_info['total'] - $order_info['prepayment']; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label>Объявленная стоимость</label>
+                            <input type="number" name="Cost" value="<?php echo $order_info['total']; ?>"
+                                   class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Описание</label>
+                            <input type="text" name="Description" value="Запчасти" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Дата отправки</label>
+                            <input type="date" name="DateTime" value="<?php echo date('Y-m-d');?>"  min="<?php echo date('Y-m-d');?>" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-info pull-right">Создать ЭН</button>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php echo form_close();?>
+
+
+
+<script>
+    function getWarehouses(Ref) {
+        $.ajax({
+            url: '/delivery/np/getWarehouses',
+            data: {Ref: Ref},
+            success: function (json) {
+                if (json) {
+                    var html = '<option>---</option>';
+                    $(json).each(function (index, item) {
+                        html += '<option value="' + item['Number'] + '">' + item['Number'] + '-' + item['Description'] + '</option>';
+                        $("[name='RecipientAddressName']").html(html).removeAttr('readonly');
+                    });
+                }
+            }
+        })
+    }
+
+    var timeout = null;
+
+    function getCity(term) {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            if (term.length > 2) {
+                $.ajax({
+                    url: '/delivery/np/searchSettlements',
+                    data: {city: term},
+                    success: function (json) {
+                        if (json) {
+                            var html = '';
+                            $(json).each(function (index, item) {
+                                console.log(item);
+                                html += '<a href="#" onclick="set(\'' + item['DeliveryCity'] + '\',\'' + item['MainDescription'] + '\',\'' + item['Area'] + '\',\'' + item['Region'] + '\');$(\'.city-res\').empty();return false;">' + item['Present'] + '</a><br>';
+                            });
+                        }
+                        $(".city-res").html(html);
+                    }
+                })
+            }
+        }, 500);
+    }
+
+    function set(DeliveryCity, RecipientCityName, RecipientArea, RecipientAreaRegions) {
+        getWarehouses(DeliveryCity);
+        $("[name='RecipientCityName']").val(RecipientCityName);
+        $("[name='RecipientArea']").val(RecipientArea);
+        $("[name='RecipientAreaRegions']").val(RecipientAreaRegions);
+    }
+
+    $(document).ready(function () {
+        $("[name='ServiceType'").change(function () {
+            if ($(this).val() == 'WarehouseDoors') {
+                $("#RecipientAddressName").hide();
+                $("#WarehouseDoors").show();
+            } else {
+                $("#RecipientAddressName").show();
+                $("#WarehouseDoors").hide();
+            }
+        });
+
+        $("[name='nalojka'").change(function(){
+           if($(this).val() == '1'){
+               $("[name='RedeliveryString']").removeAttr('disabled');
+           }else{
+               $("[name='RedeliveryString']").attr('disabled','disabled');
+
+           }
+        });
+
+        $("#np-form").submit(function(e){
+            e.preventDefault();
+            $.ajax({
+                url: $(this).attr('action'),
+                data: $(this).serialize(),
+                method: 'post',
+                success: function(response){
+                    if(response){
+                        alert(response);
+                    }else{
+                        location.reload()
+                    }
+                }
+            })
+        })
+    });
+</script>
---
+++ application/views/themes/default/delivery/np/form.php
@@ -0,0 +1,134 @@
+<div class="row">
+    <div class="panel panel-default">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>Город</label>
+                <input name="np_address" value="<?php echo @$customer->np_address;?>" type="text" onkeyup="getCity($(this).val())" class="form-control" >
+                <div class="city-res"></div>
+            </div>
+            <div class="form-group" id="form-group-RecipientAddressName" <?php if(@$customer->RecipientAddressName2){?>style="display: none;"<?php } ?>>
+                <label>Склад</label>
+                <select disabled name="RecipientAddressName" class="form-control">
+                    <?php if(isset($customer->RecipientAddressName)){?>
+                        <option value="<?php echo $customer->RecipientAddressName;?>" selected><?php echo $customer->RecipientAddressName;?></option>
+                    <?php } ?>
+                </select>
+            </div>
+            <div id="WarehouseDoors" <?php if(!@$customer->RecipientAddressName2){?>style="display: none;"<?php } ?>>
+                <div class="form-group">
+                    <div class="form-group">
+                        <label>Адрес</label>
+                        <input value="<?php echo @$customer->RecipientAddressName2;?>" class="form-control" type="text" name="RecipientAddressName2">
+                    </div>
+                    <div class="form-group">
+                        <label>Номер дома</label>
+                        <input value="<?php echo @$customer->RecipientHouse;?>" class="form-control" type="text" name="RecipientHouse">
+                    </div>
+                    <div class="form-group">
+                        <label>Этаж</label>
+                        <input value="<?php echo @$customer->RecipientFlat;?>" class="form-control" type="text" name="RecipientFlat">
+                    </div>
+                </div>
+            </div>
+            <div class="checkbox">
+                <label><input <?php if(@$customer->RecipientAddressName2){?>checked<?php } ?> type="checkbox" onchange="getField(this)">Адресная доставка</label>
+            </div>
+            <input id="DeliveryCity" type="hidden" name="DeliveryCity" value="<?php echo @$customer->DeliveryCity;?>">
+            <input type="hidden" name="RecipientCityName" value="<?php echo @$customer->RecipientCityName;?>" class="form-control">
+            <input type="hidden" name="RecipientArea" value="<?php echo @$customer->RecipientArea;?>" class="form-control">
+            <input type="hidden" name="RecipientAreaRegions" value="<?php echo @$customer->RecipientAreaRegions;?>" class="form-control">
+
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<script>
+
+    $(document).ready(function(){
+        $("#form-group-address").hide();
+
+        if($("[name='DeliveryCity']").val()){
+            var Ref = $("[name='DeliveryCity']").val();
+            var customer_warehouse = '<?php echo @$customer->RecipientAddressName;?>';
+            getWarehouses(Ref,customer_warehouse);
+        }
+
+        $("[name='np_address']").change(function(){
+            if($(this).prop('checked')){
+                $("[name='np_city'],[name='np_store']").attr('disabled',true);
+            }else{
+                $("[name='np_city'],[name='np_store']").removeAttr('disabled');
+            }
+        })
+    });
+
+    function getField(obj) {
+        if($(obj).prop('checked')){
+            $("[name='RecipientAddressName']").prop('selectedIndex',0);
+            $("#form-group-RecipientAddressName").hide();
+            $("#WarehouseDoors").show();
+        }else{
+            $("#WarehouseDoors input").each(function(){
+                $(this).val('');
+            });
+            $("#WarehouseDoors").hide();
+            $("#form-group-RecipientAddressName").show();
+        }
+    }
+
+
+    function getWarehouses(Ref,customer_warehouse) {
+        $.ajax({
+            url: '/delivery/np/getWarehouses',
+            data: {Ref:Ref},
+            success: function (json) {
+                if(json){
+                    var html = '<option>---</option>';
+                    $(json).each(function(index, item){
+                        if(item['Number'] == customer_warehouse){
+                            html += '<option selected value="'+item['Number']+'">'+item['Number']+'-'+item['Description']+'</option>';
+                        }else{
+                            html += '<option value="'+item['Number']+'">'+item['Number']+'-'+item['Description']+'</option>';
+                        }
+                       $("[name='RecipientAddressName']").html(html).removeAttr('disabled');
+                    });
+                }
+            }
+        })
+    }
+
+    var timeout = null;
+    function getCity(term){
+        clearTimeout(timeout);
+        timeout = setTimeout(function(){
+            if(term.length > 2){
+                $.ajax({
+                    url: '/delivery/np/searchSettlements',
+                    data: {city:term},
+                    success: function (json) {
+                        if(json){
+                            var html = '';
+                            $(json).each(function(index,item){
+                                console.log(item);
+                                html += '<a href="#" onclick="set(\''+item['DeliveryCity']+'\',\''+item['MainDescription']+'\',\''+item['Area']+'\',\''+item['Region']+'\');$(\'.city-res\').empty();return false;">'+item['Present']+'</a><br>';
+                            });
+                        }
+                        $(".city-res").html(html);
+                    }
+                })
+            }
+        }, 1000);
+    }
+
+    function set(DeliveryCity,RecipientCityName,RecipientArea,RecipientAreaRegions){
+        $("[name='np_address'").val(RecipientCityName + ' ' + RecipientArea + ' ' + RecipientAreaRegions);
+        $("[name='DeliveryCity']").val(DeliveryCity);
+        getWarehouses(DeliveryCity);
+        $("[name='RecipientCityName']").val(RecipientCityName);
+        $("[name='RecipientArea']").val(RecipientArea);
+        $("[name='RecipientAreaRegions']").val(RecipientAreaRegions);
+    }
+
+
+</script>
+
