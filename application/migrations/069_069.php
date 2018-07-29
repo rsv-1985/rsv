89112391641ab9551b5920683ef6d79fb58a38a9
rsv-1985 <sergey.rasputniy@gmail.com>
2018-07-29 13:14:52 +0300

    1

--- application/migrations/069_069.php
+++ application/migrations/069_069.php
@@ -14,10 +14,10 @@ class Migration_069 extends CI_Migration
         $this->db->query("ALTER TABLE `ax_customer` ADD INDEX(`email`);");
         $this->db->query("ALTER TABLE `ax_customer` ADD INDEX(`phone`);");
         $this->db->query("ALTER TABLE `ax_customer` DROP `login`;");
-        $this->db->query("UPDATE `ax_customer` SET `phone` = REPLACE(REPLACE(REPLACE(REPLACE(phone,' ',''),'(',''),')',''),'-','')");
-        $this->db->query("UPDATE `ax_order` SET `telephone` = REPLACE(REPLACE(REPLACE(REPLACE(telephone,' ',''),'(',''),')',''),'-','')");
-        $this->db->query("UPDATE `ax_vin` SET `telephone` = REPLACE(REPLACE(REPLACE(REPLACE(telephone,' ',''),'(',''),')',''),'-','')");
-        $this->db->query("UPDATE `ax_waybill_parcel` SET `telephone` = REPLACE(REPLACE(REPLACE(REPLACE(telephone,' ',''),'(',''),')',''),'-','')");
+        $this->db->query("UPDATE `ax_customer` SET `phone` = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(phone,' ',''),'(',''),')',''),'-',''),'+','')");
+        $this->db->query("UPDATE `ax_order` SET `telephone` = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(telephone,' ',''),'(',''),')',''),'-',''),'+','')");
+        $this->db->query("UPDATE `ax_vin` SET `telephone` = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(telephone,' ',''),'(',''),')',''),'-',''),'+','')");
+        $this->db->query("UPDATE `ax_waybill_parcel` SET `telephone` = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(telephone,' ',''),'(',''),')',''),'-',''),'+','')");
         $this->db->query("ALTER TABLE `ax_customer` ADD `payment_method_id` INT NOT NULL AFTER `negative_balance`;");
         $this->db->query("ALTER TABLE `ax_customer` ADD `delivery_method_id` INT NOT NULL AFTER `payment_method_id`;");
         $this->db->query("ALTER TABLE `ax_customer` ADD `additional_information` LONGTEXT NOT NULL AFTER `delivery_method_id`;");
