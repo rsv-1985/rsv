384cda53722a0e1917230fc7659fffaa6e86e57d
rsv-1985 <sergey.rasputniy@gmail.com>
2018-08-13 21:24:25 +0300

    1

--- application/views/themes/default/header.php
+++ application/views/themes/default/header.php
@@ -36,7 +36,7 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <link rel="alternate stylesheet" onload="this.rel='stylesheet'" href="<?php echo theme_url();?>style.css?v=4">
+    <link rel="alternate stylesheet" onload="this.rel='stylesheet'" href="<?php echo theme_url();?>style.css?v=5">
     <?php if($this->config->item('my_style')){?>
         <?php foreach ($this->config->item('my_style') as $style){?>
             <link rel="stylesheet" href="<?php echo $style;?>">
