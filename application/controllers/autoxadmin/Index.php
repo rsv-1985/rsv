377ab69cfc12ba69edb59885ec1b2b02344b5677
rsv-1985 <sergey.rasputniy@gmail.com>
2018-08-16 23:14:16 +0300

    Урезали до 5 историю обновлений

--- application/controllers/autoxadmin/Index.php
+++ application/controllers/autoxadmin/Index.php
@@ -21,7 +21,7 @@ class Index extends Admin_controller{
         $data['updates'] = [];
         $cache = $this->cache->file->get('updates');
         if(!$cache){
-            @exec('git log --pretty=format:"%ar : %s" -10', $output);
+            @exec('git log --pretty=format:"%ar : %s" -5', $output);
             if(is_array($output)){
                 foreach ($output as $item) {
                     $text = explode(':',$item);
