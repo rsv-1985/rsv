b449c9fa24dace07dc8a45a1e34bd528a3573fb7
rsv-1985 <sergey.rasputniy@gmail.com>
2018-08-17 08:07:17 +0300

    1

--- application/language/russian/admin/language_lang.php
+++ application/language/russian/admin/language_lang.php
@@ -6,6 +6,6 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['text_heading'] = 'Переводы';
-$lang['text_language_line'] = 'Ключь';
+$lang['text_language_line'] = 'Ключ';
 $lang['text_language_text'] = 'Текст';
 $lang['text_language_filter_placeholder'] = 'Поиск';
