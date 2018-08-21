2b666f75422ac51ad0dde612dd936e194c49bddd
rsv-1985 <sergey.rasputniy@gmail.com>
2018-08-21 22:37:36 +0300

    1

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -11,6 +11,12 @@ class MY_loader extends CI_Loader{
     public function view($view, $vars = array(), $return = FALSE)
     {
         $CI = &get_instance();
-        return $this->_ci_load(array('_ci_view' => 'themes/'.$CI->config->item('theme').'/'.$view, '_ci_vars' => $this->_ci_prepare_view_vars($vars), '_ci_return' => $return));
+        if(@file_exists('./application/views/themes/'.$CI->config->item('theme').'/MY_'.$view.'.php')){
+            $template = 'themes/'.$CI->config->item('theme').'/MY_'.$view;
+        }else{
+
+            $template = 'themes/'.$CI->config->item('theme').'/'.$view;
+        }
+        return $this->_ci_load(array('_ci_view' => $template, '_ci_vars' => $this->_ci_prepare_view_vars($vars), '_ci_return' => $return));
     }
 }
