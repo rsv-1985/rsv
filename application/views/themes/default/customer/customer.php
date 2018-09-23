dcbfe844d8580534df3b4ad2d2ed1bb91f77d1c8
rsv-1985 <sergey.rasputniy@gmail.com>
2018-09-23 21:41:06 +0300

    1

--- application/views/themes/default/customer/customer.php
+++ application/views/themes/default/customer/customer.php
@@ -15,36 +15,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
             <div class="col-md-9">
                 <h2><?php echo lang('text_orders_history');?></h2>
                 <?php if($orders){?>
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th><?php echo lang('text_orders_id');?></th>
-                            <th><?php echo lang('text_orders_date');?></th>
-                            <th><?php echo lang('text_orders_status');?></th>
-                            <th><?php echo lang('text_orders_total');?></th>
-                            <th>Статус оплаты</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach($orders as $order){?>
-                            <tr>
-                                <td><a target="_blank" href="/customer/orderinfo/<?php echo $order['id'];?>"><?php echo $order['id'];?></a></td>
-                                <td><small><?php echo $order['created_at'];?></small></td>
-                                <td><b style="color: <?php echo $status[$order['status']]['color'];?>"><?php echo $status[$order['status']]['name'];?></b></td>
-                                <td><?php echo format_currency($order['total']);?></td>
-                                <td>
-                                    <?php if(!$order['paid']){?>
-                                        <a href="/customer/pay/<?php echo $order['id'];?>">Оплатить с баланса</a>
-                                    <?php }else{?>
-                                        Оплачен
-                                    <?php } ?>
-                                </td>
-                                <td><a target="_blank" href="/customer/orderinfo/<?php echo $order['id'];?>">Подробнее</a></td>
+                    <?php foreach ($orders as $order){?>
+                        <table class="table table-condensed table-bordered">
+                            <thead>
+                            <tr class="active">
+                                <th colspan="2"><?php echo lang('text_orders_id');?>: <?php echo $order['id'];?></th>
+                                <th colspan="4"><?php echo lang('text_orders_date');?>: <?php echo $order['created_at'];?></th>
+                                <th><a target="_blank" href="/customer/orderinfo/<?php echo $order['id'];?>">Подробнее</a></th>
                             </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
+                            <tr>
+                                <td>#</td>
+                                <td>Код запчасти</td>
+                                <td>Производитель</td>
+                                <td>Наименование</td>
+                                <td>Количество</td>
+                                <td>Цена</td>
+                                <td>Статус</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            <?php $q = 1; foreach ($order['products'] as $product){?>
+                                <tr>
+                                    <td><?php echo $q;?></td>
+                                    <td><?php echo $product['sku'];?></td>
+                                    <td><?php echo $product['brand'];?></td>
+                                    <td><?php echo $product['name'];?></td>
+                                    <td><?php echo $product['quantity'];?></td>
+                                    <td><?php echo format_currency($product['price']);?></td>
+                                    <td><?php echo $status[$product['status_id']]['name'];?></td>
+                                </tr>
+                            <?php $q++; } ?>
+                            <tr class="info">
+                                <td colspan="3">Статус оплаты:<br><b>
+                                        <?php if(!$order['paid']){?>
+                                            <a href="/customer/pay/<?php echo $order['id'];?>">Оплатить с баланса</a>
+                                        <?php }else{?>
+                                            Оплачен
+                                        <?php } ?>
+                                    </b>
+                                </td>
+
+                                <td colspan="1"><?php echo lang('text_orders_status');?>:<br><b style="color: <?php echo $status[$order['status']]['color'];?>"><?php echo $status[$order['status']]['name'];?></b></td>
+                                <td colspan="3"><?php echo lang('text_orders_total');?>:<br><b><?php echo format_currency($order['total']);?></b></td>
+
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                    <?php } ?>
                     <?php echo $this->pagination->create_links();?>
                 <?php } ?>
             </div>
