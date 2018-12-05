dcc8a1452e526d374ebf796f712686bfe712c63e
rsv-1985 <Sergey.Rasputniy@gmail.com>
2018-12-05 22:10:40 +0200

    1

---
+++ application/language/russian/sto_lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['text_manufacturer'] = 'Марка автомобиля';
+$lang['text_model'] = 'Модель автомобиля';
+$lang['text_vin'] = 'VIN номер автомобиля';
+$lang['text_section_auto'] = 'Услуги и данные о автомобиле';
+$lang['text_section_date_time'] = 'Дата и время';
