12a66a27100f3f921512de09abc7d99fade8f8e5
rsv-1985 <Sergey.Rasputniy@gmail.com>
2018-12-17 14:30:12 +0200

    1

---
+++ application/language/russian/admin/customer_pay_lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Developer: Распутний Сергей Викторович
+ * Site: cms.autoxcatalog.com
+ * Email: sergey.rasputniy@gmail.com
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['text_heading'] = 'Приходные кассовые ордеры';
+$lang['text_customer_pay'] = 'Приходный кассовый ордер';
