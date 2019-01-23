a5cde71774957d1bf80f772b68cdcc426a59112b
rsv-1985 <Sergey.Rasputniy@gmail.com>
2019-01-23 13:41:35 +0200

    1

--- application/migrations/078_078.php
+++ application/migrations/078_078.php
@@ -11,7 +11,7 @@ class Migration_078 extends CI_Migration
 
     public function up()
     {
-        $this->db->query("CREATE TABLE `cmsautox`.`ax_product_images` ( `id` INT NOT NULL AUTO_INCREMENT , `product_id` INT NOT NULL , `image` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), INDEX (`product_id`)) ENGINE = MyISAM CHARSET=utf8 COLLATE utf8_general_ci;");
+        $this->db->query("CREATE TABLE `ax_product_images` ( `id` INT NOT NULL AUTO_INCREMENT , `product_id` INT NOT NULL , `image` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), INDEX (`product_id`)) ENGINE = MyISAM CHARSET=utf8 COLLATE utf8_general_ci;");
     }
 
     public function down(){
